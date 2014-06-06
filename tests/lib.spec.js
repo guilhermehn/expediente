77abeefa9397cbbe1e662413b171203433165d77
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2014-06-06 11:06:24 -0300

    remaining tests

--- tests/lib.spec.js
+++ tests/lib.spec.js
@@ -1,62 +1,60 @@
 var expect = require('expect.js')
   , lib = require('../lib')
   , expediente = require('../')
-  , simple
-  , verbose
-  , start = '10:09'
+  , start = '10:00'
   , hours = '8:00'
+  , simple = expediente(start)
+  , verbose = expediente(start, true)
 
-beforeEach(function () {
-  simple = expediente(start)
-  verbose = expediente(start, true)
-})
+describe('#expediente', function () {
+  it('should calculate the expected exit time', function () {
+    expect(simple).to.eql('19:48')
+  })
 
-describe('lib', function () {
-  describe('#expediente', function () {
-    it('should calculate the expected exit time', function () {
-      expect(simple).to.eql('19:57')
+  it('should return null if the start hour is invalid', function () {
+    expect(expediente('25:09')).to.be(null)
+  })
+
+  describe('<hours>', function () {
+    it('should accept a expedient time as second argument', function () {
+      expect(expediente(start, hours)).to.eql('18:00')
     })
 
-    it('should return null if the start hour is invalid', function () {
-      expect(expediente('25:09')).to.be(null)
+    it('should ignore invalid expedient time', function () {
+      expect(expediente(start, '27:12')).to.eql('19:48')
+      expect(expediente(start, '8:92')).to.eql('19:48')
+    })
+  })
+
+  describe('<verbose>', function () {
+    it('should return a detailed object', function () {
+      expect(verbose).to.be.a('object')
     })
 
-    describe('<hours>', function () {
-      it('should accept a expedient time as second argument', function () {
-        expect(expediente(start, hours)).to.eql('18:09')
-      })
-
-      it('should ignore invalid expedient time', function () {
-        expect(expediente(start, '27:12')).to.eql('19:57')
-        expect(expediente(start, '8:92')).to.eql('19:57')
-      })
+    it('it should return the start', function () {
+      expect(verbose.start).to.eql(start)
     })
 
-    describe('<verbose>', function () {
-      it('should return a detailed object', function () {
-        expect(verbose).to.be.a('object')
-      })
+    it('it should return the exit hour', function () {
+      expect(verbose.finish).to.eql('19:48')
+    })
 
-      it('it should return the start', function () {
-        expect(verbose.start).to.eql(start)
-      })
+    it('should return the limit time', function () {
+      expect(verbose.limit).to.eql('20:08')
+    })
 
-      it('it should return the exit hour', function () {
-        expect(verbose.finish).to.eql('19:57')
-      })
+    it('should return the remaining time', function () {
+      expect(verbose.remaining).to.eql('09:48')
+    })
 
-      it('should return the limit time', function () {
-        expect(verbose.limit).to.eql('20:17')
-      })
-
-      it('should work with all the hours argument', function () {
-        var hoursVerbose = {
-            start: start
-          , finish: '18:09'
-          , limit: '18:29'
-        }
-        expect(expediente(start, hours, true)).to.eql(hoursVerbose)
-      })
+    it('should work with all the hours argument', function () {
+      var hoursVerbose = {
+          start: start
+        , finish: '18:00'
+        , limit: '18:20'
+        , remaining: '08:00'
+      }
+      expect(expediente(start, hours, true)).to.eql(hoursVerbose)
     })
   })
 })
