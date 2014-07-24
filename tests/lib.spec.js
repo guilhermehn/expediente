e578fed61e7f791673003065c7342bf332314ff6
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2014-07-24 17:26:40 -0300

    improvements

--- tests/lib.spec.js
+++ tests/lib.spec.js
@@ -1,4 +1,5 @@
 var expect = require('expect.js')
+  , moment = require('moment')
   , lib = require('../lib')
   , expediente = require('../')
   , start = '10:00'
@@ -40,20 +41,11 @@ describe('#expediente', function () {
       expect(verbose.finish).to.eql('19:48')
     })
 
-    /*it('should return the limit time', function () {
-      expect(verbose.limit).to.eql('20:08')
-    })*/
-
-    it('should return the remaining time', function () {
-      expect(verbose.remaining).to.eql('09:48')
-    })
-
     it('should work with all the hours argument', function () {
       var hoursVerbose = {
           start: start
         , finish: '18:00'
-        // , limit: '18:20'
-        , remaining: '08:00'
+        , remaining: moment('18:00', 'HH:mm').subtract(moment()).format('HH:mm')
       }
       expect(expediente({ start: start, hours: hours, detailed: true })).to.eql(hoursVerbose)
     })
