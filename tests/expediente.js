91fb8e3561b9c1c6b53f72e1c6f7583e28b03986
Guilherme Nagatomo <gnagotomo@sp.r7.com>
2015-02-09 17:35:17 -0200

    separate tests

---
+++ tests/expediente.js
@@ -0,0 +1,99 @@
+var expect = require('expect.js');
+var moment = require('moment');
+var expediente = require('../');
+var defaults = require('../conf');
+var start = '10:00';
+
+var testHours = '8:00';
+
+var simple = expediente({
+  start: start,
+  hours: defaults.hours,
+  simple: true
+});
+
+var verbose = expediente({
+  start: start,
+  hours: defaults.hours,
+  detailed: true
+});
+
+describe('#expediente', function () {
+  it('should calculate the expected exit time', function () {
+    expect(simple).eql('19:48');
+  });
+
+  it('should return null if the start hour is invalid', function () {
+    expect(expediente('25:09')).be(null);
+  });
+
+  describe('<hours>', function () {
+    it('should accept a expedient time as second argument', function () {
+      var options = {
+        start: start,
+        hours: testHours,
+        simple: true
+      };
+
+      expect(expediente(options)).eql('18:00');
+    });
+
+    it('should return null if the expedient duration is invalid', function () {
+      var options = {
+        start: start,
+        hours: '27:12'
+      };
+
+      expect(expediente(options)).eql(null);
+    });
+  });
+
+  describe('<verbose>', function () {
+    it('should return a detailed object', function () {
+      expect(verbose).be.a('object');
+    });
+
+    it('should return the start', function () {
+      expect(verbose.start).eql(start);
+    });
+
+    it('should return the exit hour', function () {
+      expect(verbose.finish).eql('19:48');
+    });
+
+    it('should work with all the hours argument', function () {
+      var expected = {
+        start: '10:00',
+        finish: '18:00'
+      };
+
+      var options = {
+        start: start,
+        hours: testHours
+      };
+
+      var result = expediente(options);
+
+      expect(result.start).eql(expected.start);
+      expect(result.finish).eql(expected.finish);
+      expect(result.minimum).eql(expected.minimum);
+    });
+
+    it('should have a `remaining` time when it`s is before the finish time', function () {
+      var result = expediente({
+        start: start,
+        hours: testHours
+      });
+
+      var now = moment();
+      var finish = moment(result.finish, 'HH:mm');
+
+      if (finish.isAfter(now)) {
+        var remaining = moment(result.remaining, 'HH:mm');
+        var expected = moment(finish.diff(now, 'hours') + ':' + finish.diff(now, 'minutes') % 60, 'HH:mm');
+
+        expect(remaining.format('HH:mm')).eql(expected.format('HH:mm'));
+      }
+    });
+  });
+});
