a1cbae9231d65393de3812cee4a4456143a3c190
Guilherme Nagatomo <gnagotomo@sp.r7.com>
2015-03-16 17:58:13 -0300

    gulp mocha test runner

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var files = ['index.js', 'lib/*.js', 'tests/*.js', 'bin/expediente'];
 
-gulp.task('default', function () {
+gulp.task('lint', function () {
   return gulp.src(files)
     .pipe(plugins.jscs())
     .pipe(plugins.jshint('.jshintrc'))
     .pipe(plugins.jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('test', function () {
+  return gulp.src('tests/*.js').
+    pipe(plugins.mocha());
+});
+
+gulp.task('default', ['lint', 'test']);
 
 gulp.task('watch', function () {
   gulp.start('default');
