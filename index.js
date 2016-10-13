8b547ec18d49adfdeb73f85e82f71c5aba784a6b
Guilherme Nagatomo <nagatomo.guilherme@gmail.com>
2016-10-13 12:07:26 -0300

    Updated variables scoping type Fixed old add/subtract moment method usages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var moment = require('moment');
-var lib = require('./lib');
-var defaultFormat = 'HH:mm';
+const moment = require('moment')
+const lib = require('./lib')
+const DEFAULT_FORMAT = 'HH:mm'
 
 /**
  * Calculate the expedient time
@@ -8,96 +8,101 @@ var defaultFormat = 'HH:mm';
  * @return {object}
  */
 function expediente (options) {
-  var now;
-  var minimum;
-
   // Return null if the options object is invalid
   if (!lib.isValidOptions(options)) {
-    return null;
+    return null
   }
 
   // Start time moment object
-  var start = moment(options.start, defaultFormat);
+  const start = moment(options.start, DEFAULT_FORMAT)
 
   // Duration time moment object
-  var duration = moment(options.hours, defaultFormat);
+  let duration = moment(options.hours, DEFAULT_FORMAT)
 
   // If the early property is received
   // subtract it from the duration time
   if (options.early) {
-    duration = duration.subtract(moment(options.early, 'HH:mm'));
+    const earlyTime = moment(options.early, DEFAULT_FORMAT)
+    duration = duration.subtract({
+      minutes: earlyTime.minutes(),
+      hours: earlyTime.hours()
+    })
   }
 
   // Finish time moment object
-  var finish = start.clone().add(duration);
+  const finish = start.clone().add({
+    hours: duration.hours(),
+    minutes: duration.minutes()
+  })
 
   // If the output mode is set to `finish`
   // there's no need to continue, just
   // return the finish time string
   if (options.finish || options.simple) {
-    if (options.simple) {
-      console.log('Deprecation warning: the `--simple` flag will be removed from the next versions');
-      console.log('                     Use the `--finish` flag instead');
-    }
-
-    return lib.format(finish);
+    return lib.format(finish)
   }
 
   // The verbose output object starts
   // with the start and finish time
-  var result = {
+  const result = {
     start: lib.format(start),
     finish: lib.format(finish)
-  };
+  }
 
   // If the tolerance options is received
   // do the tolerance calculation
+  let minimum
   if (options.tolerance) {
     // Tolerance time moment object
-    var tolerance = moment(options.tolerance, defaultFormat);
+    const tolerance = moment(options.tolerance, DEFAULT_FORMAT)
+    const toleranceMinutes = tolerance.minutes()
+    const toleranceHours = tolerance.hours()
 
     // If the tolerance is greater than 0 minutes or 0 hours
     // calculate the minimum time and add the limit time
     // to the result object
     if (tolerance.minutes() > 0 || tolerance.hours() > 0) {
-      minimum = finish.clone().subtract(tolerance);
+      minimum = finish.clone().subtract(toleranceMinutes, 'minutes')
 
       // If the output mode is set to `minimum`
       // there's no need to continue, just
       // return the minimum time string
       if (options.minimum) {
-        return lib.format(minimum);
+        return lib.format(minimum)
       }
 
-      result.limit = lib.format(finish.clone().add(tolerance));
+      result.limit = lib.format(finish.clone().add({
+        hours: toleranceHours,
+        minutes: toleranceMinutes
+      }))
     }
 
     // Add the minimum property to the result object
-    result.minimum = lib.format(minimum);
+    result.minimum = lib.format(minimum)
   }
 
   // Get the actual time
-  now = moment().set({ seconds: 0, milliseconds: 0 });
+  const now = moment().set({ seconds: 0, milliseconds: 0 })
 
   // If now is before the finish time
   if (now.isBefore(finish)) {
     // If there's a minimum time, use it to curry the
     // remaining time calculator, otherwise use the finish time
-    var getRemaining = now.diff.bind(minimum ? minimum : finish, now);
+    const getRemaining = now.diff.bind(minimum ? minimum : finish, now)
 
     // The remaining time to be added to the result
     // with the hours and minutes remaining to the
     // minium or finish time
-    var remaining = {
+    const remaining = {
       hours: getRemaining('hours'),
       minutes: getRemaining('minutes') % 60
-    };
+    }
 
     // Add the remaining time object the actual result
-    result.remaining = lib.format(moment([remaining.hours, remaining.minutes].join(':'), defaultFormat));
+    result.remaining = lib.format(moment([remaining.hours, remaining.minutes].join(':'), DEFAULT_FORMAT))
   }
 
-  return result;
+  return result
 }
 
-module.exports = expediente;
+module.exports = expediente
